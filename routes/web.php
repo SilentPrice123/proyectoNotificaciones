4634cade86f320f8e876203549e5a39e2c9c9393
sprice123 <nando15092@gmail.com>
2023-10-11 00:49:03 -0500

    prueba rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('otras-opciones', function () {
+    return "Bienvenido a la siguiente pagina";
+});
+
+// Route::get('otras-opciones/{valor}', function ($valor) {
+//     return "Bienvenido a la opcion: $valor";
+// });
+
+Route::get('opcion/create', function () {
+    return "en esta opcion podras hacer cambios";
+});
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso,$categoria = null) {
+    if($categoria){
+        return "bienvenido al curso $curso, de la categoria: $categoria";
+    }else{
+        return "Bienvenido al curso: $curso";
+    }
+});
